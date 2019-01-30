e16757fdd3c8e21b7695563bc448aa9672410121
shuoen1997 <sli112@ucsc.edu>
2019-01-30 15:54:56 -0800

    Implements MC walking across the screen

--- PulseTest/Assets/Scripts/McMovement.cs
+++ PulseTest/Assets/Scripts/McMovement.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class McMovement : Movement
+public class McMovement : MonoBehaviour
 {
     public Animator anim;
 
-    
+    private Vector2 direction;
+    private float speed = 5f;
+    private double worldX = 21.9;
+    void Start()
+    {
+        direction = Vector2.right;
+        anim.SetBool("isWalking", true);
+    }
+
     void Update()
     {
-        anim.SetBool("isWalking", walking);
-        getInput();
-        Move();
+        transform.Translate(Time.deltaTime * speed * direction);
+        if ( transform.position.x > worldX  || transform.position.x + Mathf.Epsilon < - worldX)
+        {
+            direction *= -1;
+            transform.localScale = new Vector3(- transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+        
+        
     }
+
+    
 }
