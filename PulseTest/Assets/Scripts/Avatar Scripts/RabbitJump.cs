2dd2fcb9dae0f3a5a1f5fb75cd878c13ae5ea893
JustinP <jfpiment@ucsc.edu>
2019-02-01 15:07:47 -0800

    Fixed Right Click on Rabbit Dismount
    Corrected the rabbit carry and dismount so that it can be done repeatedly. Camera work is still in-progress.

--- PulseTest/Assets/Scripts/Avatar Scripts/RabbitJump.cs
+++ PulseTest/Assets/Scripts/Avatar Scripts/RabbitJump.cs
@@ -28,27 +28,30 @@ public class RabbitJump : MonoBehaviour
                 GetComponent<Movement>().enabled = true;
                 beingCarried = false;
             }
-            //Vector for Raycast, takes mouse position
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //Decompose to 2D vector
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
-            //Raycast hit register for mouse position
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-
-            //Check if an avatar was clicked on
-            if (hit.collider != null && hit.collider.gameObject.tag == "Avatars")
+            else
             {
-                //Check distance from object
-                Debug.Log("I want to jump into " + hit.collider.gameObject.name + "'s arms");
-                float distance = Vector3.Distance(transform.position, hit.collider.gameObject.transform.position);
-                //Debug.Log(distance);
-                if (distance < 2f)
+                //Vector for Raycast, takes mouse position
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                //Decompose to 2D vector
+                Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+                //Raycast hit register for mouse position
+                RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+                //Check if an avatar was clicked on
+                if (hit.collider != null && hit.collider.gameObject.tag == "Person")
                 {
-                    beingCarried = true;
-                    transform.parent = hit.collider.gameObject.transform;
-                    GetComponent<Movement>().enabled = false;
-                    Debug.Log("I'm being carried");
+                    //Check distance from object
+                    Debug.Log("I want to jump into " + hit.collider.gameObject.name + "'s arms");
+                    float distance = Vector3.Distance(transform.position, hit.collider.gameObject.transform.position);
+                    //Debug.Log(distance);
+                    if (distance < 2f)
+                    {
+                        beingCarried = true;
+                        transform.parent = hit.collider.gameObject.transform;
+                        GetComponent<Movement>().enabled = false;
+                        Debug.Log("I'm being carried");
+                    }
                 }
             }
             
