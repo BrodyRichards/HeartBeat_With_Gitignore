a3bf90aee7d1c47d81079acc05ad6c9020712ffa
shuoen1997 <sli112@ucsc.edu>
2019-02-01 21:37:31 -0800

    Adds progress bar (wip)

--- PulseTest/Assets/Scripts/UI Scripts/TransitStates.cs
+++ PulseTest/Assets/Scripts/UI Scripts/TransitStates.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class TransitStates : MonoBehaviour
 {
+    public Slider slider;
+    public Text text;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("In start Menu");
+        slider.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,7 +21,10 @@ public class TransitStates : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            text.gameObject.SetActive(false);
+            slider.gameObject.SetActive(true);
             StartCoroutine(LoadAsyncScene(1));
+            
         }
 
     }
@@ -25,8 +32,16 @@ public class TransitStates : MonoBehaviour
     IEnumerator LoadAsyncScene(int nextSceneIndex)
     {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextSceneIndex, LoadSceneMode.Single);
+        asyncLoad.allowSceneActivation = false;
         while (!asyncLoad.isDone)
         {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+            slider.value = progress;
+            if (progress > 0.9f)
+            {
+                asyncLoad.allowSceneActivation = true;
+            }
+
             yield return null;
         }
     }
