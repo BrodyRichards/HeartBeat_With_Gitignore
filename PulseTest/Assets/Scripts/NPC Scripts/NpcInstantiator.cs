abca8bbd93e5e4758df120a7891bd1a6d2f80751
Vienna Chan <vhchan@ucsc.edu>
2019-02-06 13:19:10 -0800

    Groupies rewritten, fixed two colliders
    Made it so that the groupies gather in a circle. Script isn't as complicated now. Changed the collider size of Groupies and Runners. Not sure if I can do the same with Loners so didn't touch that yet.

--- PulseTest/Assets/Scripts/NPC Scripts/NpcInstantiator.cs
+++ PulseTest/Assets/Scripts/NPC Scripts/NpcInstantiator.cs
@@ -21,18 +21,24 @@ public class NpcInstantiator : MonoBehaviour
     public int neighbourDistance = 100;
     public float maxForce = 0.5f;
     public float maxVelocity = 2.0f;
-    public Vector2 target;
+    public Vector3 target;
+
+    private Vector3 center;
+    public Vector3[] groupiePos;
     // Start is called before the first frame update
     void Start()
     {
         areaX = ((int)area.transform.localScale.x) / 2 - 1;
         areaY = ((int)area.transform.localScale.y) / 2 - 1;
+        int ranX = Random.Range(-areaX, areaX);
+        int ranY = Random.Range(-areaY, areaY);
+        center = new Vector3(ranX, ranY, -1);
+        groupiePos = new Vector3[groupCount];
         for (int i = 0; i < npcCount; i++) //create and instantiate the npcs (we can make it more complicated later)
         {
             int choice = Random.Range(0, 2);
-            int ranX = Random.Range(-areaX, areaX);
-            int ranY = Random.Range(-areaY, areaY);
-            target = new Vector2(ranX, ranY);
+            ranX = Random.Range(-areaX, areaX);
+            ranY = Random.Range(-areaY, areaY);
             Vector3 pos = new Vector3(ranX, ranY, -1);
             Quaternion rot = new Quaternion(0, 0, 0, 0);
             Instantiate(NPCs[choice], pos, rot);
@@ -40,11 +46,25 @@ public class NpcInstantiator : MonoBehaviour
         groupies = new GameObject[groupCount];
         for (int i = 0; i < groupCount; i++)
         {
-            int ranX = Random.Range(-areaX, areaX);
-            int ranY = Random.Range(-areaY, areaY);
+            ranX = Random.Range(-areaX, areaX);
+            ranY = Random.Range(-areaY, areaY);
             Vector3 pos = new Vector3(ranX, ranY, -1);
             groupies[i] = Instantiate(groupiePrefab, pos, Quaternion.identity) as GameObject;
+
             groupies[i].GetComponent<Groupies>().manager = this.gameObject;
+            groupies[i].GetComponent<Groupies>().target = RandomCircle(center, 3f, groupCount, i);
+
         }
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius, int ranNum, int num)
+    {
+        float deg = (float)(num + 1)/ranNum;
+        float ang = deg * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
     }
 }
