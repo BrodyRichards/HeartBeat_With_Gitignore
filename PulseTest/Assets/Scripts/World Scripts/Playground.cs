0b5ce1d037b8b5b88f86ba781f80a0195f5d8869
shuoen1997 <sli112@ucsc.edu>
2019-02-10 16:10:05 -0800

    Fixes world bound variable assignment issue

---
+++ PulseTest/Assets/Scripts/World Scripts/Playground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playground: MonoBehaviour
+{
+    private float worldX;
+    private float worldY;
+    public static float RightX = 100f;
+    public static float LeftX = -100f;
+    public static float LowerY = -25f;
+    public static float UpperY = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //worldX = GameObject.Find("/Quad").transform.localScale.x / 2;
+        //worldY = GameObject.Find("/Quad").transform.localScale.y / 2;
+        //UpperY = worldY * 0.25f;
+        //LowerY = -worldY + 15f;
+        //LeftX = -worldX + 20f;
+        //RightX = worldX - 20f;
+        //Debug.Log("uppery" + UpperY);
+        //Debug.Log("lowery" + LowerY);
+        //Debug.Log("leftx" + LeftX);
+        //Debug.Log("rightx" + RightX);
+        
+    }
+
+    
+}
