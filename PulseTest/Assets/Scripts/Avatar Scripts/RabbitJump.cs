904b9dc272475ff0f05a73da814898ccce2ef622
shuoen1997 <sli112@ucsc.edu>
2019-03-13 12:34:26 -0700

    Fixes rabbit layer

--- PulseTest/Assets/Scripts/Avatar Scripts/RabbitJump.cs
+++ PulseTest/Assets/Scripts/Avatar Scripts/RabbitJump.cs
@@ -121,7 +121,7 @@ public class RabbitJump : MonoBehaviour
         transform.parent = null;
         GetComponent<Movement>().enabled = true;
         beingCarried = false;
-        GetComponent<SortRender>().offset = 2;
+        GetComponent<SortRender>().offset = 15;
         EmoControl.rabbitHug = false;
         anim.SetBool("isCarried", false);
     }
