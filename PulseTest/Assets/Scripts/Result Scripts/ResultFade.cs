0eccade098afea3e14952e896506558b3827b1f5
NMaasch <nathanmaasch@gmail.com>
2019-03-14 17:54:19 -0700

    Result Scene Fade In

---
+++ PulseTest/Assets/Scripts/Result Scripts/ResultFade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultFade : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("4")){
+            FadeToNextLevel();
+        }
+    }
+
+    public void FadeToNextLevel()
+    {
+        //FadeToLevel();
+    }
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        //SceneManager.LoadScene(levelToLoad);
+    }
+}
