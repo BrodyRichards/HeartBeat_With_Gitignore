f1b2b2b81b7954a3f7bd756fd0dd8de4778dacbb
Vienna Chan <vhchan@ucsc.edu>
2019-04-03 20:20:52 -0700

    Thought Bubble Prototype in
    Got a dummy thought bubble to follow the MC around

---
+++ PulseTest/Assets/ThoughtBubble.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThoughtBubble : MonoBehaviour
+{
+    public GameObject mc;
+    Vector3 pos;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = mc.transform.position;
+        offset = new Vector3(5, 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = mc.transform.position;
+        transform.position = pos + offset;
+    }
+}
