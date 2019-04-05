568e69b2d213f5f94c025d3b2b736580c4b9480a
Vienna Chan <vhchan@ucsc.edu>
2019-04-05 00:07:15 -0700

    Thought bubble WIP
    Got some basic code in but definitely need to work on it more

---
+++ PulseTest/Assets/Scripts/World Scripts/ThoughtsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThoughtsManager : MonoBehaviour
+{
+    public Text thoughtText;
+    public Image thoughtBubble;
+
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void DisplayThought(Thought thought)
+    {
+        //have different string arrays in Thought.cs for different types of thoughts?
+
+    }
+
+    void hideThought()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //random events?
+        //what will trigger thoughts
+    }
+}
