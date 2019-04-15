58ebaca7daaec6b481a95dc580dbe190820b4d8f
JustinP <jfpiment@ucsc.edu>
2019-04-15 13:01:33 -0700

    Adds in behavior tree with test file
    Here is the behavior tree implementation with all the types of nodes and the test file

---
+++ PulseTest/Assets/Scripts/BT Stuff/Composites/Selector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//The selector runs until it finds any child that evaluates to SUCCESS.
+//Will return FAILURE only if each child evaluates to FAILURE
+public class Selector : Node
+{
+    //Protect list of children so that it can't be modified inadvertently
+    protected List<Node> children = new List<Node>();
+
+    //Set the list of children to one passed to the constructor
+    public Selector(List<Node> childList)
+    {
+        children.AddRange(childList);
+    }
+
+    //We are looking for the first one to evaluate to SUCCESS otherwise report FAILURE
+    public override NodeStatus Evaluate()
+    {
+        bool anyChildRunning = false;
+
+        foreach(Node node in children)
+        {
+            switch (node.Evaluate())
+            {
+                case NodeStatus.SUCCESS:
+                    currentStatus = NodeStatus.SUCCESS;
+                    return currentStatus;
+                case NodeStatus.RUNNING:
+                    anyChildRunning = true;
+                    continue;
+                case NodeStatus.FAILURE:
+                    continue;
+                default:
+                    Debug.Log("Bad value in Selector");
+                    currentStatus = NodeStatus.FAILURE;
+                    return currentStatus;
+            }
+        }
+
+        currentStatus = anyChildRunning ? NodeStatus.RUNNING : NodeStatus.FAILURE;
+
+        return currentStatus;
+    }
+}
---
+++ PulseTest/Assets/Scripts/BT Stuff/Decorators/Inverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inverter : Node
+{
+    //This is the child node that the inverter wraps
+    private Node child;
+
+    //This is an accessor so that you can get the child node from the inverter
+    //If you want to access it call it by node, e.g. myInverter.node
+    public Node node
+    {
+        get
+        {
+            return child;
+        }
+    }
+
+    //Requires that the child node for inversion be passed to constructor
+    public Inverter(Node n)
+    {
+        child = n;
+    }
+
+    //On SUCCESS return FAILURE
+    //On FAILURE return SUCCESS
+    //On RUNNING return RUNNING
+    public override NodeStatus Evaluate()
+    {
+        switch (child.Evaluate())
+        {
+            case NodeStatus.SUCCESS:
+                currentStatus = NodeStatus.FAILURE;
+                return currentStatus;
+            case NodeStatus.FAILURE:
+                currentStatus = NodeStatus.SUCCESS;
+                return currentStatus;
+            case NodeStatus.RUNNING:
+                currentStatus = NodeStatus.RUNNING;
+                return currentStatus;
+            default:
+                Debug.Log("Bad value in Inverter");
+                return NodeStatus.FAILURE;
+        }
+    }
+}
