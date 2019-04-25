d6dfd27c976a3453aece13729278f9593d9094fe
Vienna Chan <vhchan@ucsc.edu>
2019-04-25 12:54:49 -0700

    Variable Moved to CameraScript
    Here ya go Justin

--- PulseTest/Assets/Scripts/World Scripts/ThoughtBubble.cs
+++ PulseTest/Assets/Scripts/World Scripts/ThoughtBubble.cs
@@ -7,7 +7,7 @@ public class ThoughtBubble : MonoBehaviour
 {
     public GameObject mc;
     Vector3 pos;
-    Vector3 offset;
+    Vector3 offset, offset2, scale, scaleOpp;
 
     
     // Start is called before the first frame update
@@ -15,20 +15,33 @@ public class ThoughtBubble : MonoBehaviour
     {
         pos = mc.transform.position;
         if ("SampleScene" == SceneManager.GetActiveScene().name)
-        {
+        { 
             offset = new Vector3(-8, 5, 0);
+            offset2 = new Vector3(8, 5, 0);
         }
-        else
+        else //for the tutorial scene
         {
             offset = new Vector3(-3, 2, 0);
-        }      
+        }
+        scale = transform.localScale;
+        scaleOpp = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z); 
     }
 
     // Update is called once per frame
     void Update()
     {
         pos = mc.transform.position;
-        transform.position = pos + offset;
-        
+        /*
+        if (Vector3.Distance(transform.position, GameObject.Find("LeftBound").transform.position) <= 50)
+        {
+            transform.localScale = scaleOpp;            //make it flip
+            transform.position = pos + offset2;
+        }
+        */
+        //else if (Vector3.Distance(transform.position, GameObject.Find("LeftBound").transform.position) >= 50)
+        //{
+            transform.localScale = scale;
+            transform.position = pos + offset;
+        //}     
     }
 }
