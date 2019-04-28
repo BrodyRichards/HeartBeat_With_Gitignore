281cce0a1f84a9a46b177b37920a6daee865a635
NMaasch <nathanmaasch@gmail.com>
2019-04-28 12:56:07 -0700

    Control S2

--- PulseTest/Assets/Scripts/UI Scripts/S2Effect.cs
+++ PulseTest/Assets/Scripts/UI Scripts/S2Effect.cs
@@ -5,10 +5,13 @@ using System.Collections;
 [ExecuteInEditMode]
 public class S2Effect : MonoBehaviour{
     public Material material;
-
+    public float gradient;
+    public float colorT;
     // Postprocess the image
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        material.SetFloat("_GradThresh",gradient);
+        material.SetFloat("_ColorThreshold",colorT);
         Graphics.Blit(source, destination, material);
     }
 }
