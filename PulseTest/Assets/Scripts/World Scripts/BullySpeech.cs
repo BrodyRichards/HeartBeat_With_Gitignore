fe5d15ec24eeb78f76137e4cf23adac2bbbc948d
Vienna Chan <vhchan@ucsc.edu>
2019-05-14 17:26:15 -0700

    Bully kid WIP
    Bully backend wip

---
+++ PulseTest/Assets/Scripts/World Scripts/BullySpeech.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BullySpeech : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Hey new kid
+        //What a stupid hat
+        //Go back to your old school
+        //Hey Shrimp
+        //Hey carrotboy
+    }
+}
