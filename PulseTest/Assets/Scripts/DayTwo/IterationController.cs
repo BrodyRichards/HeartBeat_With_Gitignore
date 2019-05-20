ed40f4e3b52d5ce07c6362b7ebdda085995f58b2
Justin <jpimntel@gmail.com>
2019-05-19 18:17:13 -0700

    Fixed Var Set Error
    Fixed an error in IterationController that was causing a variable to not be set

--- PulseTest/Assets/Scripts/DayTwo/IterationController.cs
+++ PulseTest/Assets/Scripts/DayTwo/IterationController.cs
@@ -29,16 +29,17 @@ public class IterationController : MonoBehaviour
     public void PrepareNextDay()
     {
         //If there is one that is least used then we need to alternate which avatar is disabled
-        if (dayCount >= 3)
+        /*if (dayCount >= 3)
         {
             //Replace the avatar that is missing
             leastPlayed.transform.localScale = new Vector3(1, 1, 1);
-        }
+        }*/
 
         //Have to keep this array in order
         leastUsed = FindLeastPlayed(bunnyTimer, ballKidTimer, musicKidTimer);
         //Convert int to string for GameObject lookup
         string lUsed = leastUsed.ToString();
+        //Debug.Log(lUsed);
         //Find GameObject
         leastPlayed = GameObject.Find(lUsed);
         //Disable in hierarchy
@@ -58,6 +59,7 @@ public class IterationController : MonoBehaviour
             if (currLowest <= 0f)
             {
                 currLowest = avTimes[i];
+                leastIndex = i + 1;
             }
 
             if(avTimes[i] < currLowest)
