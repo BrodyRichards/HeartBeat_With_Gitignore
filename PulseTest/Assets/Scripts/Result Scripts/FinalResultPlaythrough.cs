75d0aa646b43809700311665af14c115aa182bd8
Vienna Chan <vhchan@ucsc.edu>
2019-06-01 13:19:56 -0700

    Final First Run
    Made a prototype version of the final run through. Is it any good? Who knows

---
+++ PulseTest/Assets/Scripts/Result Scripts/FinalResultPlaythrough.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalResultPlaythrough : MonoBehaviour
+{
+    public GameObject leftPage;
+    private Animator leftAnim;
+    public GameObject rightPage;
+    private Animator rightAnim;
+
+    public Image bubble;
+
+    private float time;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0f;
+        timer = 0f;
+
+        leftAnim = leftPage.GetComponent<Animator>();
+        rightAnim = rightPage.GetComponent<Animator>();
+
+        Invoke("playAnim", 10f);
+        Invoke("dialogue", 5f);
+    }
+
+    private void playAnim()
+    {
+        leftAnim.Play("JournalLeftPage");
+        rightAnim.Play("JournalRightPage");
+    }
+    
+    private void dialogue()
+    {
+        timer = time + 3.5f;
+        bubble.GetComponent<Image>().gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        time = Time.fixedUnscaledTime;
+        if (time >= timer)
+        {
+            bubble.GetComponent<Image>().gameObject.SetActive(false);
+        }
+    }
+}
