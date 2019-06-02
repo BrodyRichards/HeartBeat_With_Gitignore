6d35e65cce8c60dba59bc4fd798dfaa75726559b
Vienna Chan <vhchan@ucsc.edu>
2019-06-02 14:28:10 -0700

    Revert "Start Screen Journal Open"
    This reverts commit 3a50bd0d73da347a5fe25e4096b9c44d449db9e9.

--- PulseTest/Assets/Scripts/UI Scripts/TransitStates.cs
+++ PulseTest/Assets/Scripts/UI Scripts/TransitStates.cs
@@ -11,10 +11,6 @@ public class TransitStates : MonoBehaviour
     public GameObject[] starIndicators;
     private int currentIndex;
 
-    public GameObject startJournal;
-    public GameObject leftSide;
-    public GameObject rightSide;
-
 
     // Start is called before the first frame update
     void Start()
@@ -36,16 +32,6 @@ public class TransitStates : MonoBehaviour
     }
 
     public void StartGame()
-    {
-        startJournal.SetActive(false);
-        leftSide.SetActive(true); rightSide.SetActive(true);
-        leftSide.GetComponent<Animator>().Play("LeftJournalOpen");
-        rightSide.GetComponent<Animator>().Play("RightJournalOpen");
-        Invoke("NextScene", 3f);
-        //SceneManager.LoadScene(1);
-    }
-
-    public void NextScene()
     {
         SceneManager.LoadScene(1);
     }
