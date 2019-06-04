a3472c21d302c39302433f4a70cb03e33818329e
Shuo-En Li <sli112@ucsc.edu>
2019-06-03 23:44:19 -0700

    fixes tutorial load taking forever
    - now the black screen time decreases

--- PulseTest/Assets/Scripts/Tutorial Scripts/TutorialFade.cs
+++ PulseTest/Assets/Scripts/Tutorial Scripts/TutorialFade.cs
@@ -7,14 +7,24 @@ public class TutorialFade : MonoBehaviour
 {
     public Animator fadeAnimator;
     private int levelToLoad;
+    private bool isReady;
+    private bool isCalledOnce;
+
+    private void Awake()
+    {
+        isReady = false;
+        isCalledOnce = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (McExit.nextScene)
+        if (McExit.nextScene && !isCalledOnce)
         {
 
             FadeToLevel();
+            StartCoroutine(LoadNextSceneAsync("LoadingScene"));
+            isCalledOnce = true;
         }   
     }
 
@@ -30,7 +40,7 @@ public class TutorialFade : MonoBehaviour
 
     public void OnFadeComplete()
     {
-        SceneManager.LoadScene("loadingScene");
+        isReady = true;
     }
 
     public void FadeOut(){
@@ -44,6 +54,28 @@ public class TutorialFade : MonoBehaviour
     }
     public void FadeIn(){
         fadeAnimator.Play("Fade_In",0,0f);
+    }
+
+    IEnumerator LoadNextSceneAsync(string nstl)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(nstl);
+        ao.allowSceneActivation = false;
+        while (!ao.isDone)
+        {
+            //Output the current progress
+            Debug.Log(ao.progress);
+
+            // Check if the load has finished
+            if (ao.progress >= 0.9f)
+            {
+                //Wait to you press the space key to activate the Scene
+                if (isReady)
+                    //Activate the Scene
+                    ao.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
     }
 
 }
