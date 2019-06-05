12a139efb364619f01a15709f71041776ca8a2a7
Vienna Chan <vhchan@ucsc.edu>
2019-06-04 17:41:30 -0700

    Music Kid now Talks
    Speech bubble added for Music Kid

---
+++ PulseTest/Assets/MusicKidBubble.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicKidBubble : MonoBehaviour
+{
+    public Image musicBubble;
+    Vector3 pos;
+    Vector3 offset;
+
+    private float time;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicBubble.GetComponent<Image>().gameObject.SetActive(false);
+        offset = new Vector3(-4f, 5f, 0f);
+        time = 0f;
+        timer = 0f;
+    }
+
+    void setTimer()
+    {
+        time = Time.fixedUnscaledTime;
+        timer = time + 2.5f;
+        BallProjectile.musicKidTalk = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.fixedUnscaledTime;
+        pos = NpcInstantiator.musicKidPos;
+        musicBubble.transform.position = pos + offset;
+        if (time >= timer)
+        {
+            musicBubble.GetComponent<Image>().gameObject.SetActive(false);
+        }
+        if (BallProjectile.musicKidTalk)
+        {
+            //Debug.Log("Sneezed " + NPCs.sneeze);
+            setTimer();
+            musicBubble.GetComponent<Image>().gameObject.SetActive(true);
+        }
+    }
+}
