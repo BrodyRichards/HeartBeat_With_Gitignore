64128ae4cd79eec043258a86996e8ee5f7e7fc5d
Vienna Chan <vhchan@ucsc.edu>
2019-06-06 14:43:19 -0700

    "Nostalgic" Bubble Removed
    Yeah

--- PulseTest/Assets/Scripts/Result Scripts/FinalResultPlaythrough.cs
+++ PulseTest/Assets/Scripts/Result Scripts/FinalResultPlaythrough.cs
@@ -11,23 +11,24 @@ public class FinalResultPlaythrough : MonoBehaviour
     public GameObject rightPage;
     private Animator rightAnim;
 
-    public Image bubble;
+    
+    //public Image bubble;
 
     private float time;
-    private float timer;
-
+    //private float timer;
+    
     private float endTimer = 20f;
     // Start is called before the first frame update
     void Start()
     {
         time = 0f;
-        timer = 0f;
+        //timer = 0f;
 
         leftAnim = leftPage.GetComponent<Animator>();
         rightAnim = rightPage.GetComponent<Animator>();
 
         Invoke("playAnim", 10f);
-        Invoke("dialogue", 5f);
+        //Invoke("dialogue", 5f);
     }
 
     private void playAnim()
@@ -36,20 +37,22 @@ public class FinalResultPlaythrough : MonoBehaviour
         rightAnim.Play("JournalRightPage");
     }
     
+    /*
     private void dialogue()
     {
         timer = time + 3.5f;
         bubble.GetComponent<Image>().gameObject.SetActive(true);
     }
-
+    */
     private void Update()
     {
         time = Time.timeSinceLevelLoad;
+        /*
         if (time >= timer)
         {
             bubble.GetComponent<Image>().gameObject.SetActive(false);
         }
-
+        */
         if (time >= endTimer)
         {
             SceneManager.LoadScene("CreditScreen");
